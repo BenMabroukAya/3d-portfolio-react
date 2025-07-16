2eb8546ef2d4b5a8fb2487fb6539705aa538f90c
BenMabroukAya <aya.benmabrouk@isimg.tn>
2025-07-16 23:01:22 +0100

    update Tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,3 @@
-
 import { technologies } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { BallCanvas } from "./canvas";
@@ -6,7 +5,7 @@ import { BallCanvas } from "./canvas";
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {technologies.slice(0, 12).map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
