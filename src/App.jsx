afa45cae9c88b8b403ff85949dca570d0220b308
BenMabroukAya <aya.benmabrouk@isimg.tn>
2025-07-17 20:25:43 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, SocialMedia, StarsCanvas, Tech, Works } from "./components";
 
 const App = () => {
   return (
@@ -21,8 +21,13 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
+        <SocialMedia />
       </div>
+      <footer className="text-center text-sm text-gray-500 py-4">
+        Made with ❤️ By Aya BEN MABROUK
+      </footer>
     </BrowserRouter>
+    
   );
 }
 
