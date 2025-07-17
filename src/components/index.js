37b70b7f46ef01ca43eca22fcdb75eb5085dc8b1
BenMabroukAya <aya.benmabrouk@isimg.tn>
2025-07-17 20:24:59 +0100

    update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,13 +5,14 @@ import Experience from './Experience';
 import Feedbacks from './Feedbacks';
 import Hero from './Hero';
 import Navbar from './Navbar';
+import SocialMedia from './SocialMedia';
 import Tech from './Tech';
 import Works from './Works';
-
 
 export {
   About, BallCanvas,
   ComputersCanvas, Contact,
   EarthCanvas, Experience, Feedbacks, Hero,
-  Navbar, StarsCanvas, Tech, Works
+  Navbar, SocialMedia, StarsCanvas, Tech, Works
 };
+
